8271b5f0afdcf81891d216e53316fdaab8b51238
ghifarirmdn <ghifariramadhan2001@gmail.com>
2023-07-12 16:35:11 +0700

    recap UI

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -21,6 +21,10 @@
                 href="{{ route('users') }}">
                 <i class="fa-solid fa-users"></i><span class="mx-3">Users</span>
             </a>
+            <a class="flex items-center px-6 py-2 mt-4 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ request()->is('recap*') ? 'text-white bg-gray-700' : 'text-gray-500' }}"
+                href="{{ route('recap') }}">
+                <i class="fa-solid fa-list"></i></i><span class="mx-3">Recap</span>
+            </a>
         @endif
         
         <a href="{{ route('performance') }}" class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ request()->is('performance*') ? 'text-white bg-gray-700' : 'text-gray-500' }}">
