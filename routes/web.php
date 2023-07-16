2df26a2b5482c44ee7415afb6d8af5387d80a5dc
iostream-code <aryakmifjannah2603@gmail.com>
2023-07-17 00:38:50 +0700

    adding delete button for users

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,4 @@ Route::get('/user/create', [UserController::class, 'create'])->name('create_user
 Route::post('/user/store', [UserController::class, 'store'])->name('store_user');
 Route::get('/user/{user}', [UserController::class, 'show'])->name('show_user');
 Route::patch('/user/{user}/update', [UserController::class, 'update'])->name('update_user');
+Route::delete('/user/{user}/delete', [UserController::class, 'delete'])->name('delete_user');
