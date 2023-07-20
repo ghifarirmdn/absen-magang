3f5f0667671c4b28edbd125a33fac312e7c19687
iostream-code <aryakmifjannah2603@gmail.com>
2023-07-20 13:23:22 +0700

    adding store permission letter

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PermissionController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $file = $request->file('permission_letter');
+        $path = $file->getClientOriginalExtension();
+
+        Storage::disk('local')->put('public/files/' . $path, file_get_contents($file));
+
+        Permission::create([
+            'user_id' => Auth::id(),
+            'category' => $request->category,
+            'permission_letter' => $path,
+        ]);
+
+        return redirect()->route('home');
+    }
 }
